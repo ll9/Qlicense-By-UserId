e2f2ac84758c4ca0aba72d2f80f17d1f6b18c728
ll9 <llwierer@web.de>
2018-11-28 22:16:56 +0100

    First failed test

--- DemoWinformAppTests/Utils/LicenseManagerTests.cs
+++ DemoWinformAppTests/Utils/LicenseManagerTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using DemoWinFormApp.Utils;
+using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +12,27 @@ namespace DemoWinformAppTests.Utils
     [TestFixture]
     class LicenseManagerTests
     {
+        private const string LicenseFile = "licensfile";
+        LicenseManager _licenseManager;
+        Mock<IFileHandler> _fileHandler;
+       
+
+        [SetUp]
+        public void Init()
+        {
+            _fileHandler = new Mock<IFileHandler>();
+            _licenseManager = new LicenseManager(LicenseFile, _fileHandler.Object);
+        }
+
+        [Test]
+        public void CheckLicense_LicenseExists_ReturnTrue()
+        {
+            _fileHandler
+                .Setup(fh => fh.FileExists(It.IsAny<string>()))
+                .Returns(true);
+
+            _licenseManager.CheckLicense();
+            _fileHandler.Verify(f => f.FileExists(It.IsAny<string>()));
+        }
     }
 }
